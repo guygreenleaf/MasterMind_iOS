83e50d6c490fbb43569a70ea1adf759bd4979fc4
guygreenleaf <guygreenleaf@icloud.com>
2021-03-03 08:47:22 -0800

    Initial Commit

---
+++ Mastermind/MastermindApp.swift
@@ -0,0 +1,17 @@
+//
+//  MastermindApp.swift
+//  Mastermind
+//
+//  Created by Guy Greenleaf on 3/3/21.
+//
+
+import SwiftUI
+
+@main
+struct MastermindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
